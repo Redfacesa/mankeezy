fac87e32666a75c59c48ba37f758a61951afd9ec
Red face <redfacesa@gmail.com>
2026-08-18 07:23:27 +0200

    njn

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -3,7 +3,10 @@ import { NAV } from '../lib/links';
 export default function SiteNav() {
   return (
     <nav className="site-nav">
-      <a href="#birthday" className="site-nav-logo script">Mankeezy</a>
+      <a href="#birthday" className="site-nav-brand">
+        <img src="/mankeezy-logo.png" alt="Mankeezy" className="site-nav-logo-img" />
+        <span className="site-nav-logo script">Mankeezy</span>
+      </a>
       <div className="site-nav-links">
         {NAV.map((item) => (
           <a
